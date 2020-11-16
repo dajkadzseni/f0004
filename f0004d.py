40ced68094511220a12ce57889ce7e764daee633
dajkadzseni <71691882+dajkadzseni@users.noreply.github.com>
2020-11-16 23:33:43 +0100

    Delete f0004d.py

--- f0004d.py
+++
@@ -1,5 +0,0 @@
-magyar_mérföld = input('Hány magyar mérföldre van a sárkány barlangja? ')
-magyar_mérföld = int(magyar_mérföld)
-km = magyar_mérföld * 8.3536
-tengeri_mérföld = km / 1.852
-print('Ez a táv', km, 'kilométert és', tengeri_mérföld, 'tengeri mérföldet jelent.')
